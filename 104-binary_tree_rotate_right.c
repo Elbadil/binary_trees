8957e3e9cee2ad28931395466e423a6acbb01538
Elbadil <elbaliadil@gmail.com>
2023-09-01 19:44:19 +0100

    Rotate right

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -8,23 +8,23 @@
 
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
-    binary_tree_t *new_root;
-    binary_tree_t *temp;
+	binary_tree_t *new_root;
+	binary_tree_t *temp;
 
-    if (tree == NULL || tree->left == NULL)
-        return (tree);
+	if (tree == NULL || tree->left == NULL)
+		return (tree);
 
-    new_root = tree->left;
-    temp = new_root->right;
+	new_root = tree->left;
+	temp = new_root->right;
 
-    new_root->parent = tree->parent;
-    tree->left = temp;
+	new_root->parent = tree->parent;
+	tree->left = temp;
 
-    new_root->right = tree;
-    tree->parent = new_root;
+	new_root->right = tree;
+	tree->parent = new_root;
 
-    if (temp != NULL)
-        temp->parent = tree;
+	if (temp != NULL)
+		temp->parent = tree;
 
-    return (new_root);
+	return (new_root);
 }
